22fb00443a9f1aee17530ed838201a89f58a63fe
Peter Braden <peterbraden@peterbraden.co.uk>
2013-09-29 08:53:12 -0700

    wip

--- js/raytracer.js
+++ js/raytracer.js
@@ -3,33 +3,6 @@ var plib = require('./plib.js')
 require('setimmediate')
 
 module.exports = rays = {};
-
-rays.Camera = function(location, lookat, angle, width, height, up){
-	this.up = up || [0.0,1.0,0.0];
-	this.location = location;
-	this.lookat = lookat;
-	this.angle = angle;
-	
-	this.camz = plib.v3.normalise(plib.v3.sub(lookat, location));
-	this.camx = plib.v3.normalise(plib.v3.cross(this.up, this.camz));
-	this.camy = plib.v3.normalise(plib.v3.cross(this.camx, plib.v3.sub([0,0,0], this.camz)));
-	
-	
-	this.tax = Math.tan(angle);
-	this.tay = Math.tan((height/width) * angle)
-	
-	this.getRay = function(x,y){
-		var xdir = plib.v3.scale(this.camx, (x - 0.5) * this.tax);
-		var ydir = plib.v3.scale(this.camy, (y - 0.5) * this.tay);
-		
-		var pt = plib.v3.add(this.camz, plib.v3.add(xdir, ydir));
-		
-		//postMessage("DBG2 " + this.location + " |  " +pt + " | " + plib.v3.sub(pt, this.location) );
-		return [this.location, pt]
-	
-	}
-	
-}
 rays.intToColor = function(i){
 	var c = [i % 256, (i>>8) % 256, (i>>16) % 256]
 	return 'rgb(' + parseInt(c[0]) + "," + parseInt(c[1]) + "," + parseInt(c[2]) + ')';
